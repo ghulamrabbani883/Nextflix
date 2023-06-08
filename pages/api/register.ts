dba43590e5b4e1139f0a3e09b9f30bb403834c61
adityagoyal200 <goyaladitya3815@gmail.com>
2023-06-08 12:12:47 +0530

    working

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,37 +1,39 @@
 import bcrypt from 'bcrypt';
 import { NextApiRequest, NextApiResponse } from 'next';
-import prismadb from '@/lib/prismadb';
+import prismadb from '@/libs/prismadb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
     if (req.method !== 'POST') {
-        return res.status(405).end();
+      return res.status(405).end();
     }
-    try {
-        const { email, name, password } = req.body;
 
-        const existinguser = await prismadb.user.findUnique({
-            where: {
-                email,
-            }
-        });
-        if (existinguser) {
-            return res.status(422).json({ error: "Email taken" });
-        }
+    const { email, name, password } = req.body;
 
-        const hashedPassword = await bcrypt.hash(password,12);
+    const existingUser = await prismadb.user.findUnique({
+      where: {
+        email
+      }
+    })
 
-        const user = await prismadb.user.create({
-            data:{
-                email,
-                name,
-                hashedPassword,
-                image: '',
-                emailVerified: new Date(),
-            }
-        });
-        return res.status(200).json(user);
-    } catch (error) {
-        console.log(error)
-        return res.status(400).end();
+    if (existingUser) {
+      return res.status(422).json({ error: 'Email taken' });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: '',
+        emailVerified: new Date(),
+      }
+    })
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(400).json({ error: `Something went wrong: ${error}` });
+  }
 }
