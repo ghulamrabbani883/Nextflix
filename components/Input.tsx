2cca9af7534a94fc74cd4032398a402f90d3fbfe
adityagoyal200 <goyaladitya3815@gmail.com>
2023-06-09 18:34:08 +0530

    back

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 interface InputProps {
   id: string;
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  onChange: any;
   value: string;
   label: string;
   type?: string;
@@ -10,41 +11,50 @@ const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
   return (
     <div className="relative">
       <input
-        id={id}
-<<<<<<< HEAD
-        className="
-          block
-          rounded-md
-          px-6
-          pt-4
-          pb-1
-          w-full
-          text-md
-          text-black
-          appearance-none
-          focus:outline-none
-          focus:ring-0
-          peer
-        "
-        placeholder=" "
         onChange={onChange}
-        type={type}
-=======
->>>>>>> a5be511c00a63f6192defcdd6bff169277108513
         value={value}
         type={type}
-        onChange={onChange}
-        className="block w-full px-6 pt-6 pb-1 text-white rounded-md appearance-none text-md focus:outline-none focus:ring-0 peer"
+        id={id}
+        className="
+        block
+        rounded-md
+        px-6
+        pt-6
+        pb-1
+        w-full
+        text-md
+      text-white
+      bg-neutral-700
+        appearance-none
+        focus:outline-none
+        focus:ring-0
+        peer
+        invalid:border-b-1
+        "
+        placeholder=" " 
       />
-      <label
-        className="absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3"
-        htmlFor={id}
-      >
-        {label}
-      </label>
+      <label 
+        htmlFor={id} 
+        className="
+        absolute 
+        text-md
+      text-zinc-400
+        duration-150 
+        transform 
+        -translate-y-3 
+        scale-75 
+        top-4 
+        z-10 
+        origin-[0] 
+        left-6
+        peer-placeholder-shown:scale-100 
+        peer-placeholder-shown:translate-y-0 
+        peer-focus:scale-75
+        peer-focus:-translate-y-3
+      ">{label}</label>
     </div>
-  );
-};
+  )
+}
 
 export default Input;
 
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,11 @@
-import { useState, useCallback } from "react";
-import { useRouter } from "next/router";
-import { signIn } from "next-auth/react";
-import axios from "axios";
-import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
-import Input from "@/components/Input";
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+import { NextPageContext } from 'next';
+import { getSession, signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+import Input from '@/components/Input';
 
 const Auth = () => {
   const router = useRouter();
@@ -50,9 +51,9 @@ const Auth = () => {
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className="w-full h-full bg-black lg:bg-opacity-50">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img src="/images/logo.png" alt="logo" className="h-12" />
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
